f62ab82405bce930f26de7a18733ae119a5b854a
Oussama Ait Benalla <oussama@aitbenalla.com>
2021-11-17 22:20:28 +0100

    update

---
+++ public/assets/js/current-link.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('a.nav-link').each(function(){
+        if ($(this).prop('href') == window.location.href) {
+            $(this).addClass('active'); $(this).parents('li').addClass('active');
+        }
+    });
+});
