0e6c7b79036dfb7146f27298dafe3d4831756ceb
Oussama Ait Benalla <oussama@aitbenalla.com>
2021-11-18 18:38:12 +0100

    add controller and smarty engine

--- public/index.php
+++ public/index.php
@@ -1,8 +1,22 @@
 <?php
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+use App\Controller\AppController;
+$request = $_SERVER['REQUEST_URI'];
+$app = new AppController();
 
-use App\Model\Test;
-use App\Model\Database;
-
-$db = new Database();
+switch ($request) {
+    case '/' :
+        $app->index();
+        break;
+    case '/list' :
+        $app->list();
+        break;
+    case '/add':
+        $app->add();
+        break;
+    default:
+        http_response_code(404);
+        $app->error();
+        break;
+}
